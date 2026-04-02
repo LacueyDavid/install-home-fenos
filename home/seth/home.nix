af96b9356c53ba172b0e1fddc52a2620b3f5aafc
LacueyDavid <lacuey.david@gmail.com>
2026-04-03 01:15:37 +0200

    feat: add Hyprland Wayland desktop baseline

--- home/seth/home.nix
+++ home/seth/home.nix
@@ -14,9 +14,69 @@
 
   programs.zsh.enable = true;
 
+  xdg.enable = true;
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+    settings = {
+      "$mod" = "SUPER";
+      monitor = ",preferred,auto,1";
+
+      exec-once = [
+        "waybar"
+        "mako"
+      ];
+
+      input = {
+        kb_layout = "fr";
+        follow_mouse = 1;
+        touchpad.natural_scroll = true;
+      };
+
+      general = {
+        gaps_in = 4;
+        gaps_out = 8;
+        border_size = 2;
+        layout = "dwindle";
+        "col.active_border" = "rgb(89b4fa)";
+        "col.inactive_border" = "rgb(585b70)";
+      };
+
+      decoration = {
+        rounding = 8;
+      };
+
+      bind = [
+        "$mod, Return, exec, kitty"
+        "$mod, Q, killactive,"
+        "$mod, F, fullscreen, 1"
+        "$mod, R, exec, wofi --show drun"
+        "$mod, V, togglefloating,"
+        "$mod, M, exit,"
+        "$mod, left, movefocus, l"
+        "$mod, right, movefocus, r"
+        "$mod, up, movefocus, u"
+        "$mod, down, movefocus, d"
+      ];
+
+      bindm = [
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+      ];
+    };
+  };
+
   home.packages = with pkgs; [
     htop
     ripgrep
     fd
+    kitty
+    waybar
+    wofi
+    mako
+    wl-clipboard
+    grim
+    slurp
   ];
 }
--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -20,6 +20,39 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
 
+  networking.networkmanager.enable = true;
+
+  services.xserver.enable = false;
+  services.greetd = {
+    enable = true;
+    settings.default_session = {
+      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+      user = "greeter";
+    };
+  };
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-hyprland
+      pkgs.xdg-desktop-portal-gtk
+    ];
+  };
+
+  security.polkit.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+  };
+
   users.users.seth = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
@@ -31,5 +64,13 @@
     git
     neovim
     wget
+    kitty
+    waybar
+    wofi
+    mako
+    hyprpaper
+    wl-clipboard
+    grim
+    slurp
   ];
 }
